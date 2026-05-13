2447a6d69c9165b5128c99211237503772259d59
tokorom <tokorom@gmail.com>
2026-05-13 10:02:25 +0900

    Fix url

--- Casks/promptflow.rb
+++ Casks/promptflow.rb
@@ -2,7 +2,7 @@ cask "promptflow" do
   version "0.3.0"
   sha256 "346b373118ef0aad3a5e42727b265c288ce594fa36dec112f2c93b98ce4f4311"
 
-  url "https://github.com/tokorom/PromptFlow/releases/download/0.3.0/PromptFlow-0.3.0.dmg"
+  url "https://github.com/tokorom/PromptFlow/releases/download/#{version}/PromptFlow-#{version}.dmg"
   name "PromptFlow"
   desc "Draft, manage, and submit prompts from anywhere on macOS"
   homepage "https://github.com/tokorom/PromptFlow"
