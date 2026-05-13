234aae184df4b18729a7754dfeeb3612a4bacdbf
tokorom <tokorom@gmail.com>
2026-05-13 12:06:21 +0900

    Revert "Update prompttap to 0.4.1"
    This reverts commit ff127d930417d96e40f327d7608130b729307dc4.

--- Casks/prompttap.rb
+++ Casks/prompttap.rb
@@ -1,5 +1,5 @@
 cask "prompttap" do
-  version "0.4.1"
+  version "0.4.0"
   sha256 "21a9db30734a029e4724ae033f5751309c19db32d7df3a264dfaad59b89c5088"
 
   url "https://github.com/tokorom/PromptTap/releases/download/#{version}/PromptTap-#{version}.dmg"
