c6be49a3c71da8d239272ef8946707f918339d84
tokorom <tokorom@gmail.com>
2026-05-13 09:50:38 +0900

    Add promptflow.rb

---
+++ promptflow.rb
@@ -0,0 +1,11 @@
+cask "promptflow" do
+  version "0.3.0"
+  sha256 "346b373118ef0aad3a5e42727b265c288ce594fa36dec112f2c93b98ce4f4311"
+
+  url "https://github.com/tokorom/PromptFlow/releases/download/0.3.0/PromptFlow-0.3.0.dmg"
+  name "PromptFlow"
+  desc "Draft, manage, and submit prompts from anywhere on macOS"
+  homepage "https://github.com/tokorom/PromptFlow"
+
+  app "PromptFlow.app"
+end
