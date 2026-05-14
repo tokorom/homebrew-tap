56ddabdd5e968dbfc2d23cb1b3384febbd089e10
tokorom <tokorom@gmail.com>
2026-05-14 20:15:55 +0900

    Update prompttap to 0.13.0

--- Casks/prompttap.rb
+++ Casks/prompttap.rb
@@ -1,6 +1,6 @@
 cask "prompttap" do
-  version "0.12.0"
-  sha256 "ea9733c0a72fa5e10531ea4230eef198dca1bc8411e81fde315dadec70e035fa"
+  version "0.13.0"
+  sha256 "efa10f0d3c59adfe4a0c789ef3413ead228a3d3711225a12cec7735f39f928a0"
 
   url "https://github.com/tokorom/PromptTap/releases/download/#{version}/PromptTap-#{version}.dmg"
   name "PromptTap"
