551dfc3159ab0c93c44db4f6b46760f478bc2672
tokorom <tokorom@gmail.com>
2026-05-18 18:43:47 +0900

    Add Casks/flowlauncher.rb

---
+++ Casks/flowlauncher.rb
@@ -0,0 +1,11 @@
+cask "flowlauncher" do
+  version "0.1.0"
+  sha256 "xxx"
+
+  url "https://github.com/tokorom/FlowLauncher/releases/download/#{version}/FlowLauncher-#{version}.dmg"
+  name "FlowLauncher"
+  desc "Super simple app launcher"
+  homepage "https://github.com/tokorom/FlowLauncher"
+
+  app "FlowLauncher.app"
+end
