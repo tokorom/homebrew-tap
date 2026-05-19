af51faefad4525457becd26c5a0e8c2da53b5707
tokorom <tokorom@gmail.com>
2026-05-19 13:00:56 +0900

    Update flowlauncher to 0.14.0

--- Casks/flowlauncher.rb
+++ Casks/flowlauncher.rb
@@ -1,6 +1,6 @@
 cask "flowlauncher" do
-  version "0.13.0"
-  sha256 "5bc1a527f2b3a0a4f3d9b4b1bd449e47585b674cd14825837b8f510fc71bc605"
+  version "0.14.0"
+  sha256 "fedaa50586943737a8e5ba771b04e48b5f5f53d2649c16b119195e3194dbd489"
 
   url "https://github.com/tokorom/FlowLauncher/releases/download/#{version}/FlowLauncher-#{version}.dmg"
   name "FlowLauncher"
